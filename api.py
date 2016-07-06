74c3fca2839ac46884906f384b7cb61780f52114
anu-sha <anusha.mogallapu@gmail.com>
2016-07-06 16:41:06 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,6 @@
 
 
-"""api.py - Create and configure the Game API exposing the resources.
-This can also contain game logic. For more complex games it would be wise to
-move game logic to another file. Ideally the API will be simple, concerned
-primarily with communication to/from the API's users."""
+"""api.py - Tic-Tac-Toe API resources."""
 
 
 import logging
